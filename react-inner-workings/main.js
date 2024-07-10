ddb3a9b3d96bb37acdfcaac203397247f981d003
rokib97 <hasanrokib1997@gmail.com>
2024-07-10 20:57:42 +0600

    make react app manually

--- react-inner-workings/main.js
+++ react-inner-workings/main.js
@@ -1,3 +1,27 @@
-console.log("react..app");
-console.log(React);
-console.log(ReactDOM);
+// const HelloWorld = () => {
+//   return React.createElement("p", { style: { color: "red" } }, "Hello World");
+// };
+// const App = () => {
+//   return React.createElement(
+//     "h1",
+//     { style: { color: "red" } },
+//     "This is aa fokira react app",
+//     HelloWorld()
+//   );
+// };
+const HelloWorld = () => {
+  return <h1>Hello World !</h1>;
+};
+
+const App = () => {
+  return (
+    <>
+      <div>This is a fokira react app</div>
+      <HelloWorld />
+    </>
+  );
+};
+
+const Root = ReactDOM.createRoot(document.getElementById("root"));
+
+Root.render(<App />);
