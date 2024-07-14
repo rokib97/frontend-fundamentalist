5a7423a02bbbf4f63920a5e7302f69242d048007
rokib97 <hasanrokib1997@gmail.com>
2024-07-15 03:21:54 +0600

    cleanup function

--- react-hook-optimization/src/components/UseEffectExample.tsx
+++ react-hook-optimization/src/components/UseEffectExample.tsx
@@ -3,20 +3,30 @@ import { useEffect, useState } from "react";
 const UseEffectExample = () => {
   const [hidden, setHidden] = useState(false);
 
-  useEffect(() => {
-    console.log("render");
-
-    return () => {
-      console.log("cleanup running..");
-    };
-  }, [hidden]);
   return (
     <div>
       <button onClick={() => setHidden((prevState) => !prevState)}>
-        Click ME
+        {hidden ? "Show" : "Hide"}
       </button>
+
+      {!hidden && <Counter />}
     </div>
   );
+};
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      console.log("rendering..");
+      setCount((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+  return <h1 className="border border-red-500 p-5">{count}</h1>;
 };
 
 export default UseEffectExample;
