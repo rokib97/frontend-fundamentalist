5db5c6ee154a2c67a78928152435f8da31ad4ba7
rokib97 <hasanrokib1997@gmail.com>
2024-07-15 03:46:24 +0600

    cleanup function with fetch

--- react-hook-optimization/src/components/UseEffectExample.tsx
+++ react-hook-optimization/src/components/UseEffectExample.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 const UseEffectExample = () => {
-  const [hidden, setHidden] = useState(false);
+  const [hidden, setHidden] = useState(true);
 
   return (
     <div>
@@ -9,24 +9,39 @@ const UseEffectExample = () => {
         {hidden ? "Show" : "Hide"}
       </button>
 
-      {!hidden && <Counter />}
+      {!hidden && <Todo />}
     </div>
   );
 };
 
-const Counter = () => {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      console.log("rendering..");
-      setCount((prev) => prev + 1);
-    }, 1000);
+// const Counter = () => {
+//   const [count, setCount] = useState(0);
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       console.log("rendering..");
+//       setCount((prev) => prev + 1);
+//     }, 1000);
 
+//     return () => {
+//       clearInterval(intervalId);
+//     };
+//   }, []);
+//   return <h1 className="border border-red-500 p-5">{count}</h1>;
+// };
+
+const Todo = () => {
+  const controller = new AbortController();
+  const signal = controller.signal;
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/1`, { signal }).then(
+      (res) => res.json().then((data) => alert(data.title))
+    );
     return () => {
-      clearInterval(intervalId);
+      controller.abort();
     };
   }, []);
-  return <h1 className="border border-red-500 p-5">{count}</h1>;
+
+  return <div>todo...</div>;
 };
 
 export default UseEffectExample;
